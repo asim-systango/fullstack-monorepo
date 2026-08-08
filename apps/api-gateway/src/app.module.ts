0e32a0adb1b71e42fc9902523cc25da561efb53d
ritik.joshi <rjoshi@systango.com>
2026-08-08 14:49:59 +0530

    revert: remove existing user and auth module along with related table

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -2,11 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { JwtAuthGuard, RolesGuard } from './common/auth';
 import { databaseConfig } from './config';
-import { AuthModule } from './modules/auth';
 import { HealthModule } from './modules/health';
-import { UsersModule } from './modules/users';
 
 const db = databaseConfig();
 
@@ -26,14 +23,8 @@ const db = databaseConfig();
     TypeOrmModule.forRoot({
       ...db,
     }),
-    UsersModule,
-    AuthModule,
     HealthModule,
   ],
-  providers: [
-    { provide: APP_GUARD, useClass: ThrottlerGuard },
-    { provide: APP_GUARD, useClass: JwtAuthGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
-  ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
--- apps/api-gateway/src/modules/users/index.ts
+++
@@ -1,3 +0,0 @@
-export { UsersModule } from './users.module';
-export { UsersService } from './users.service';
-export { User, type UserRole } from './user.entity';
